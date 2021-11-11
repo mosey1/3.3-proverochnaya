0f2f57f5658b289bbbd4050b09fb04460d21f210
Dababy <Dababy@DESKTOP-79IU4EK>
2021-11-11 13:21:43 +0200

    Добавьте файлы проекта.

---
+++ 3.3 proverochnaya/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3._3_proverochnaya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите x:");
+                double x = double.Parse(Console.ReadLine());
+                double S = 1;
+                for (int i = 1; i <= n; i++)
+                {
+                    long F = 1;
+                    for (int j = 1; j <= i; j++) F *= j;
+                    S += Math.Cos(i * x) / F;
+                }
+                Console.WriteLine("S={0:F2}", S);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
